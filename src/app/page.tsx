a05af061ce3812139f70dc5eeffbda5e51c1e2f2
Timothy Attah <timothyattah733@gmail.com>
2023-08-17 15:07:38 +0100

    changed a comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,6 +48,7 @@ export default function Home() {
         >
           <h2>
             Docs <span>-&gt;</span>
+            Go.
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
