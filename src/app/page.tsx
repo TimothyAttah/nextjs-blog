1f115802e2b7151449602642d748441f4afb1e88
Timothy Attah <timothyattah733@gmail.com>
2023-09-06 22:52:50 +0100

    Added new comment again and h4 tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,6 +77,7 @@ export default function Home() {
           <h2>Templates...</h2>
           <p>Explore the Next.js 13 playground.</p>
           <p>Exploring the playground.</p>
+          <h4>This is an h four tag</h4>
         </a>
 
         <a
