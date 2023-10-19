b5ceb7c9b1a62c58dc779ced3be9ead953e16856
Timothy Attah <timothyattah733@gmail.com>
2023-10-19 22:29:50 +0100

    Added a new prayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -129,6 +129,10 @@ export default function Home() {
             Oh yes. Today is another day Oh Lord. Let your Mercy come upon us oh
             Lord!
           </p>
+          <p>
+            Thank you Lord for today. I know your grace will be upon me tomorrow
+            and upon my family. From now on in Jesus name. Amen.
+          </p>
         </div>
         <div>
           <h1>Thank you Lord God for the journey mercy of my mum. </h1>
