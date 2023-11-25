f5710b0b989d018ea56ba9c92ce1b11a210e52dd
Timothy Attah <timothyattah733@gmail.com>
2023-11-25 22:52:17 +0100

    Added a comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,7 +126,7 @@ export default function Home() {
           <p>Thank God for today.</p>
           <h1>Thank God for journey mercies.</h1>
           <p>God help me. And help us too.</p>
-          <h1>Thank you Lord for today.</h1>
+          <h1>Thank you Lord for today!!!</h1>
 
           <p>
             Oh yes. Today is another day Oh Lord. Let your Mercy come upon us oh
